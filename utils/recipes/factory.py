fdab8a937f719ce400fc1b4302e86f749d9b1e5c
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-05-19 14:42:42 -0300

    Teste sync repositorio

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -3,14 +3,11 @@ from random import randint
 
 from faker import Faker
 
-
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
 
-
 fake = Faker('pt_BR')
 # print(signature(fake.random_number))
-
 
 def make_recipe():
     return {
@@ -33,7 +30,6 @@ def make_recipe():
             'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
         }
     }
-
 
 if __name__ == '__main__':
     from pprint import pprint
