9a7555f98f99761343fead43e6406ce1961a1222
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-05-20 01:13:44 -0300

    seção 7 concluída

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -3,14 +3,18 @@ from random import randint
 
 from faker import Faker
 
+
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
+
 
 fake = Faker('pt_BR')
 # print(signature(fake.random_number))
 
+
 def make_recipe():
     return {
+        'id': fake.random_number(digits=2, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
@@ -30,6 +34,7 @@ def make_recipe():
             'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
         }
     }
+
 
 if __name__ == '__main__':
     from pprint import pprint
