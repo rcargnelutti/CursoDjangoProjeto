8648187e6fc8e5460572204cdf75c540fca399ee
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-05-23 14:43:57 -0300

    58 e 59 - QuerySet e Filter receitas is_published

--- recipes/models.py
+++ recipes/models.py
@@ -1,13 +1,15 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -22,7 +24,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
