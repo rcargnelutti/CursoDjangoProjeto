ea61940733d63f918a370e2649ca05d5b3f6b205
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-05-27 11:38:54 -0300

    64 - página de detalhes e debug

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
-from django.http import Http404
 from django.shortcuts import get_list_or_404, render
-from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
@@ -30,7 +28,12 @@ def category(request, category_name):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
