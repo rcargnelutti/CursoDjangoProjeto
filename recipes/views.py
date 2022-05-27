1e4a3eb898f181e62efc6e97c8153c86c28b1154
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-05-27 10:36:19 -0300

    Limpando so exemplos

--- recipes/views.py
+++ recipes/views.py
@@ -16,20 +16,6 @@ def home(request):
 
 
 def category(request, category_name):
-    # Exemplo com query set
-    # recipes = Recipe.objects.filter(
-    #     category__name=category_name,
-    #     is_published=True,
-    # ).order_by('-id')
-
-    # if not recipes:
-    #     raise Http404('Not found')
-
-    # Exemplo com lista
-    # recipes = get_list_or_404(
-    #    Recipe, category__name=category_name, is_published=True)
-
-    # exemplo com lista e query set
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__name=category_name,
