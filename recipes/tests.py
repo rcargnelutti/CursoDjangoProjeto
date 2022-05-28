138a93ba52897e3523367549e340b878d29f186e
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-05-28 16:24:35 -0300

    74

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.urls import reverse
 
 
 class RecipeURLsTest(TestCase):
-    def test_is_ok(self):
-        print('ola mundo')
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
