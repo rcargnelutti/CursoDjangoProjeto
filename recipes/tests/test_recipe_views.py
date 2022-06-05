b0d0d21f834785df59f8b5033e60da86c28dbc38
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-05 18:31:33 -0300

    78

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -16,3 +16,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_views_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
