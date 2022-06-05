f69c42a178365f1e79ad3d1a6e89a4026a0a4b13
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-05 18:56:19 -0300

    79

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -20,3 +20,14 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here</h1>',
+            response.content.decode('utf-8')
+        )
