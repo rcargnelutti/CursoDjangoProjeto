d6ca934b2fa8c737b6bd97da6109cda49582412f
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-09 16:09:45 -0300

    95

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -32,7 +32,7 @@ class RecipeTestBase(TestCase):
         author_data=None,
         title='Recipe Title',
         description='Recipe Description',
-        slug='recipe-slug',
+        slug='recipe-slug55',
         preparation_time=10,
         preparation_time_unit='Minutos',
         servings=5,
