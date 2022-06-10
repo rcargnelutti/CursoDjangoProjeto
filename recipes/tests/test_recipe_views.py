b34b61b3a4cf5325e3d79a5ad2f2821dc9a7dbca
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-10 10:50:24 -0300

    Tests funcionaram apos instalar pytest-django

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -7,22 +7,22 @@ from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         # Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse('recipes:home'))
@@ -34,7 +34,7 @@ class RecipeViewsTest(RecipeTestBase):
         # Tenho que escrever mais algumas coisas sobre o test
         # self.fail('Para que eu termine de digitá-lo')
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_home_template_loads_recipes(self):
         # Need a recipe for this test
         self.make_recipe()
@@ -47,7 +47,7 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('Recipe Title', content)
         self.assertEqual(len(response_context_recipes), 1)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_home_template_dont_load_recipes_not_published(self):
         """Test recipe is_published False dont show"""
         # Need a recipe for this test
@@ -61,16 +61,16 @@ class RecipeViewsTest(RecipeTestBase):
             response.content.decode('utf-8')
         )
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_name': 'cafe'}))
         self.assertIs(view.func, views.category)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:category', kwargs={'category_name': 'PEIXE'}))
+            reverse('recipes:category', kwargs={'category_name': 'cafe'}))
         self.assertEqual(response.status_code, 404)
 
     @skip('PULANDO TESTES')
@@ -86,7 +86,7 @@ class RecipeViewsTest(RecipeTestBase):
         # Check if one recipe exists
         self.assertIn(needed_title, content)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_category_template_dont_load_recipes_not_published(self):
         """Test recipe is_published False dont show"""
         # Need a recipe for this test
@@ -97,12 +97,12 @@ class RecipeViewsTest(RecipeTestBase):
 
         self.assertEqual(response.status_code, 404)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_detail_views_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         # Recipe.objects.get(pk=1).delete()
         response = self.client.get(
@@ -110,7 +110,7 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_detail_template_loads_the_correct_recipe(self):
         needed_title = 'This is a detail page - It load one recipe'
         # Need a recipe for this test
@@ -123,7 +123,7 @@ class RecipeViewsTest(RecipeTestBase):
         # Check if one recipe exists
         self.assertIn(needed_title, content)
 
-    @skip('PULANDO TESTES')
+    # @skip('PULANDO TESTES')
     def test_recipe_detail_template_dont_load_recipe_not_published(self):
         """Test recipe is_published False dont show"""
         # Need a recipe for this test
