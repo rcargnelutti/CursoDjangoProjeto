a76ca699e62d806a9e8ed76f99bf082ef088efd9
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-13 14:40:04 -0300

    109

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -46,7 +47,14 @@ def search(request):
     search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
-        'search_term': search_term
+        'search_term': search_term,
+        'recipes': recipes,
     })
