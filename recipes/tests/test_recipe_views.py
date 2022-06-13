b16a2bcfad4342d63ce6d3c406e4742d67b5f70c
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-13 11:59:39 -0300

    107

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,4 +1,5 @@
 from unittest import skip
+from urllib import response
 
 from django.urls import resolve, reverse
 from recipes import views
@@ -148,3 +149,11 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_search_raises_404_if_no_search_term(self):
         response = self.client.get(reverse('recipes:search'))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;&lt;Teste&gt;&quot;',
+            response.content.decode('utf-8')
+        )
