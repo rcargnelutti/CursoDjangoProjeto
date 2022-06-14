6406baa0ca069ffdbbd9fd8ec8bbbb705bdcd761
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-14 11:29:04 -0300

    117 - Paginando Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -9,9 +10,14 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
     return render(request, 'recipes/pages/home.html', context={
         # 'recipes': [make_recipe() for _ in range(10)],
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
