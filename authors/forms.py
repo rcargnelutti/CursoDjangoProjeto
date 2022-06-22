6b008f876199fa52806d0d8b14843f423c29e9bf
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-06-22 15:37:56 -0300

    134

--- authors/forms.py
+++ authors/forms.py
@@ -13,3 +13,28 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # exclude = ['first_name']
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+                'invalid': 'This field is invalid'
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here',
+            })
+        }
