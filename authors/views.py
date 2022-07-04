34244b9fa4a64873fc8e0829c76d7c56722e44b5
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-07-04 17:04:28 -0300

    157

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -37,7 +37,11 @@ def register_create(request):
 
 
 def login_view(request):
-    return render(request, 'authors/pages/login.html')
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
 
 
 def login_create(request):
