d32d77b1fece8afa33e8a563a84a93a70cd862b4
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-07-07 10:41:46 -0300

    169

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,4 +1,6 @@
 
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -7,7 +9,10 @@ from .base import RecipeBaseFunctionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
+        # self.make_recipe_in_batch(qtd=20)
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
+        # self.sleep()
         self.assertIn('No recipes found here', body.text)
