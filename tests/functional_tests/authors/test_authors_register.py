61efe3889d3f11c3e40c703d04bf81434feb74f0
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-07-07 17:05:29 -0300

    175

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -5,10 +5,6 @@ from .base import AuthorsBaseTest
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def get_by_placeholder(self, web_element, placeholder):
-        return web_element.find_element(
-            By.XPATH, f'//input[@placeholder="{placeholder}"]'
-        )
 
     def fill_form_dummy_data(self, form):
 
