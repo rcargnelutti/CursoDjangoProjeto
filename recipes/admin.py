9dfbcf9e3051e839b441bc0588fed43ea9ecf327
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-07-08 15:00:42 -0300

    181

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
+
 from .models import Category, Recipe
 
 # Register your models here.
 
+
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'title', 'created_at', 'is_published', 'author']
+    list_display_links = ['id', 'title', 'created_at']
+    search_fields = ['id', 'title', 'description', 'slug', 'preparation_steps']
+    list_filter = ['category', 'author',
+                   'is_published', 'preparation_steps_is_html']
+    list_per_page = 25
+    list_editable = ['is_published']
+    ordering = ['-id']
+
 
 admin.site.register(Category, CategoryAdmin)
