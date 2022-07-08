c35535c0947da2635fbe7a20f8e3d0acb5336248
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-07-08 09:13:53 -0300

    176

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -37,3 +37,14 @@ class AuthorsLoginTest(AuthorsBaseTest):
 
         # end test
         # self.sleep()
+
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url +
+            reverse('authors:login_create')
+        )
+
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
