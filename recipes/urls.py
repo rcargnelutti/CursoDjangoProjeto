4a35806051a9c73c9f63f20561b525a43b5b462f
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-17 13:46:11 -0300

    204

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,7 @@ from . import views  # from recipes.views import home
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.RecipeListViewBase.as_view(), name="home"),
     path('recipes/search/', views.search, name="search"),
     path('recipes/category/<str:category_name>/',
          views.category, name="category"),
