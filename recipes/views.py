3ba2977a6f9fe33049177db7be842b816c7f7ba3
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-19 17:01:10 -0300

    235

--- recipes/views.py
+++ recipes/views.py
@@ -14,9 +14,16 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def theory(request, *args, **kwargs):
+    recipes = Recipe.objects.all()
+    recipes = recipes.filter(title__icontains='Receita')
+
+    context = {
+        'recipes': recipes
+    }
     return render(
         request,
-        'recipes/pages/theory.html'
+        'recipes/pages/theory.html',
+        context=context
     )
 
 
