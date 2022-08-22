fee8014721fa80314f7de95593221b16704026a1
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-22 11:48:54 -0300

    Add favicon

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +26,7 @@ urlpatterns = [
     path('authors/', include('authors.urls')),
     path('recipes/', include('recipes.urls')),  # dominio.com/recipes
     path('__debug__/', include('debug_toolbar.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
