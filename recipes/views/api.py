fc3b7f9fab094208c997cfb97156ea47a49401f0
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-29 11:02:15 -0300

    298

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+# from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from tag.models import Tag
 
 from ..models import Recipe
@@ -41,42 +42,65 @@ class RecipeAPIv2List(ListCreateAPIView):
     #     )
 
 
-class RecipeAPIv2Detail(APIView):
-    def get_recipe(self, pk):
-        recipe = get_object_or_404(
-            Recipe.objects.get_published(),
-            pk=pk
-        )
-        return recipe
+class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
 
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            many=False,
-            context={'request': request},
-        )
-        return Response(serializer.data)
+    # SOBRESCREVENDO METODOS
+    # def partial_update(self, request, *args, **kwargs):
+    #     pk = kwargs.get('pk')
+    #     recipe = self.get_queryset().filter(pk=pk).first()
+    #     serializer = RecipeSerializer(
+    #         instance=recipe,
+    #         data=request.data,
+    #         many=False,
+    #         context={'request': request},
+    #         partial=True, # Atualização parcial
+    #     )
+    #     serializer.is_valid(raise_exception=True)
+    #     print('MUDEI O PARTIAL UPDATE')
+    #     serializer.save()
+    #     return Response(
+    #         serializer.data,
+    #     )
+
+# class RecipeAPIv2Detail(APIView):
+#     def get_recipe(self, pk):
+#         recipe = get_object_or_404(
+#             Recipe.objects.get_published(),
+#             pk=pk
+#         )
+#         return recipe
+
+#     def get(self, request, pk):
+#         recipe = self.get_recipe(pk)
+#         serializer = RecipeSerializer(
+#             instance=recipe,
+#             many=False,
+#             context={'request': request},
+#         )
+#         return Response(serializer.data)
     
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data=request.data,
-            many=False,
-            context={'request': request},
-            partial=True, # Atualização parcial
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
-        )
+#     def patch(self, request, pk):
+#         recipe = self.get_recipe(pk)
+#         serializer = RecipeSerializer(
+#             instance=recipe,
+#             data=request.data,
+#             many=False,
+#             context={'request': request},
+#             partial=True, # Atualização parcial
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(
+#             serializer.data,
+#         )
     
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         recipe = self.get_recipe(pk)
+#         recipe.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
