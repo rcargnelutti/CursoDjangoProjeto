e4087aa1c1a0cff722b9fbc40b5ae146c319ea25
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-29 16:32:38 -0300

    307

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,9 +1,10 @@
 from itertools import permutations
 
 from django.shortcuts import get_object_or_404
+from recipes.permissions import IsOwner
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
@@ -20,7 +21,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
     def get_serializer_class(self):
@@ -44,6 +45,29 @@ class RecipeAPIv2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        return super().get_permissions()
+
+    def list(self, request, *args, **kwargs):
+        print('REQUEST', request.user)
+        print(request.user.is_authenticated)
+        return super().list(request, *args, **kwargs)
     
 
 @api_view()
