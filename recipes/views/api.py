633da17e9534dff804c89a9b63edf43cbb57bf55
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-29 11:03:20 -0300

    298 - Removendo comentarios

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -20,26 +20,6 @@ class RecipeAPIv2List(ListCreateAPIView):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
-    # def get(self, request):
-    #     recipes = Recipe.objects.get_published()[:10]
-    #     serializer = RecipeSerializer(
-    #         instance=recipes,
-    #         many=True,
-    #         context={'request': request},
-    #     )
-    #     return Response(serializer.data)
-    
-    # def post(self, request):
-    #     serializer = RecipeSerializer(
-    #         data=request.data,
-    #         context={'request': request},
-    #     )
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(
-    #         serializer.data,
-    #         status=status.HTTP_201_CREATED
-    #     )
 
 
 class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
@@ -64,43 +44,6 @@ class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
     #     return Response(
     #         serializer.data,
     #     )
-
-# class RecipeAPIv2Detail(APIView):
-#     def get_recipe(self, pk):
-#         recipe = get_object_or_404(
-#             Recipe.objects.get_published(),
-#             pk=pk
-#         )
-#         return recipe
-
-#     def get(self, request, pk):
-#         recipe = self.get_recipe(pk)
-#         serializer = RecipeSerializer(
-#             instance=recipe,
-#             many=False,
-#             context={'request': request},
-#         )
-#         return Response(serializer.data)
-    
-#     def patch(self, request, pk):
-#         recipe = self.get_recipe(pk)
-#         serializer = RecipeSerializer(
-#             instance=recipe,
-#             data=request.data,
-#             many=False,
-#             context={'request': request},
-#             partial=True, # Atualização parcial
-#         )
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(
-#             serializer.data,
-#         )
-    
-#     def delete(self, request, pk):
-#         recipe = self.get_recipe(pk)
-#         recipe.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
