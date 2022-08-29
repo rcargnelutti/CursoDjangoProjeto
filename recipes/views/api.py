6bbf4b21bd01824f05da2dd90864b3baaf00fd73
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-29 11:15:30 -0300

    298 - removendo os imports desnecessários

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,11 +1,10 @@
 from django.shortcuts import get_object_or_404
-# from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import (ListCreateAPIView,
                                      RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
 
 from ..models import Recipe
