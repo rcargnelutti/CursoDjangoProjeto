556e0b4ff83aa2ed71356463cbd25739d170420f
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-29 15:05:15 -0300

    306

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,6 +1,9 @@
+from itertools import permutations
+
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
@@ -17,6 +20,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticated, ]
 
 
     def get_serializer_class(self):
