d7282cdbf44c5947f48071f55f4c6aff46af72f5
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-29 14:11:37 -0300

    302

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -19,6 +19,29 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     pagination_class = RecipeAPIv2Pagination
 
 
+    def get_serializer_class(self):
+        return super().get_serializer_class()
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["example"] = 'this is in context now'
+        return context
+
+    # sobrescrevendo querysey para filtrar/buscar recipes por category
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
+    
+
 @api_view()
 def tag_api_detail(request, pk):
     tag = get_object_or_404(
