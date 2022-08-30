631b7491429c1ce01ff853b54c3840bc86758ddb
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-30 11:15:54 -0300

    320

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -10,10 +10,27 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         api_url = reverse_result or reverse('recipes:recipes-api-list')
         return api_url
 
+
     def get_recipe_api_list(self, reverse_result=None):
         api_url = self.get_recipe_reverse_url(reverse_result)
         response = self.client.get(api_url)
         return response
+
+
+    def get_jwt_access_token(self):
+        userdata = {
+            'username': 'user',
+            'password': 'password'
+        }
+        self.make_author(
+            username=userdata.get('username'),
+            password=userdata.get('password')
+        )
+        response = self.client.post(
+            reverse('recipes:token_obtain_pair'), data={**userdata}
+        )
+        return response.data.get('access')
+
 
     def test_recipe_api_list_returns_status_code_200(self):
         response = self.get_recipe_api_list()
@@ -21,6 +38,7 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             response.status_code,
             200
         )
+
 
     @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
     def test_recipe_api_list_loads_correct_number_of_recipes(self):
@@ -40,6 +58,7 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             qtd_of_loaded_recipes
         )
 
+
     def test_recipe_api_list_do_not_show_not_published_recipes(self):
         recipes = self.make_recipe_in_batch(qtd=2)
         recipe_not_published = recipes[0]
@@ -50,6 +69,7 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             len(response.data.get('results')),
             1
         )
+
 
     @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=10)
     def test_recipe_api_list_loads_recipes_by_category_id(self):
@@ -81,6 +101,7 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             9
         )
 
+
     def test_recipe_api_list_user_must_send_jwt_token_to_create_recipe(self):
         api_url = self.get_recipe_reverse_url()
         response = self.client.post(api_url)
@@ -88,3 +109,7 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             response.status_code,
             401
         )
+
+
+    def test_jwt_login(self):
+        print(self.get_jwt_access_token())
