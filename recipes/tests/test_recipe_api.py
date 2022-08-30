4551bdba23e07abc127b2b3a6e66ac648d271f86
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2022-08-30 11:28:13 -0300

    321

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -32,6 +32,18 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         return response.data.get('access')
 
 
+    def get_recipe_raw_data(self):
+        return {
+            'title': 'This is the title',
+            'description': 'This is the description',
+            'preparation_time': 1,
+            'preparation_time_unit': 'Minutes',
+            'servings': '1',
+            'servings_unit': 'Person',
+            'preparation_steps': 'This is the preparation steps.'
+        }
+
+
     def test_recipe_api_list_returns_status_code_200(self):
         response = self.get_recipe_api_list()
         self.assertEqual(
@@ -111,5 +123,16 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         )
 
 
-    def test_jwt_login(self):
-        print(self.get_jwt_access_token())
+    def test_recipe_api_list_logged_user_can_create_a_recipe(self):
+        data = self.get_recipe_raw_data()
+        # data['preparation_time'] = -1
+        response = self.client.post(
+            self.get_recipe_reverse_url(),
+            data=data,
+            HTTP_AUTHORIZATION=f'Bearer {self.get_jwt_access_token()}'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            201
+        )
